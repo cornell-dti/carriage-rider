841cdb3884b1dd43d718b0713831a0cad9661782
Tony Yang <txy3@cornell.edu>
2020-02-09 17:10:26 -0500

    setup Login.dart

---
+++ lib/Login.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+import 'dart:convert';
+import 'package:google_sign_in/google_sign_in.dart';
+
+GoogleSignIn _googleSignIn = GoogleSignIn(
+  scopes: [
+    'email',
+    'https://www.googleapis.com/auth/userinfo.profile',
+  ],
+);
+
+_handleSignIn() async {
+  try {
+    await _googleSignIn.signIn();
+  } catch (error) {
+    print(error);
+  }
+}
+
+Future<String> tokenFromAccount(GoogleSignInAccount account) async {
+  GoogleSignInAuthentication auth;
+  try {
+    auth = await account.authentication;
+    print('okay');
+  } catch (error) {
+    print('error');
+  }
+  return auth.idToken;
+}
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  GoogleSignInAccount currentUser;
+  bool success;
+
+  setCurrentUser(GoogleSignInAccount account) {
+    setState(() {
+      currentUser = account;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    currentUser = null;
+    success = false;
+    try {
+      _googleSignIn.signInSilently(); }
+    catch (error) {
+      _googleSignIn.signIn();
+    }
+    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount account) {
+      setCurrentUser(account);
+      tokenFromAccount(currentUser).then((token) async {
+        return await authenticationRequest(token);
+      }).then((response) {
+        var json = jsonDecode(response);
+        setState(() {
+          success = json['success'] == 'true' ? true : false;
+        });
+        print(json['success']);
+        return json['success'];
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // add guard based on backend verification later
+    if (currentUser == null) {
+      return Scaffold(
+          body: Container(
+              color: Colors.white,
+              child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      FlutterLogo(size: 150),
+                      SizedBox(height: 50),
+                      SignInButton()
+                    ],
+                  ))));
+    } else {
+    }
+  }
+}
+
+class SignInButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return OutlineButton(
+        splashColor: Colors.grey,
+        onPressed: () {
+          _handleSignIn();
+        },
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+        highlightElevation: 0,
+        borderSide: BorderSide(color: Colors.grey),
+        child: Padding(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image(
+                    image: AssetImage('assets/images/google_logo.png'),
+                    height: 35.0),
+                Padding(
+                  padding: EdgeInsets.only(left: 10),
+                  child: Text(
+                    'Sign in with Google',
+                    style: TextStyle(fontSize: 20, color: Colors.grey),
+                  ),
+                )
+              ],
+            )));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:io';
+import 'dart:convert' as convert;
+import 'Login.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,9 +13,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Carriage Rider',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(title: 'Carriage Rider'),
+          primarySwatch: Colors.blue,
+          fontFamily: 'SFPro',
+          accentColor: Color.fromRGBO(60, 60, 67, 0.6),
+          textTheme: TextTheme(headline: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            subhead: TextStyle(fontSize: 17.0, fontWeight: FontWeight.bold),
+            display1: TextStyle(fontSize: 17.0, color: Colors.black),
+          )),
+      home: Login()
     );
   }
 }
@@ -45,4 +54,18 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+}
+
+String localhost() {
+  if(Platform.isAndroid) {
+    return 'http://10.0.2.2:3000'; // works for emulator
+  } else {
+    return 'http://localhost:3000';
+  }
+}
+
+authenticationRequest(String token) async {
+  var endpoint = localhost() + '/verify';
+  Response response = await post(endpoint, body: {"token": token});
+  return response.body;
 }
