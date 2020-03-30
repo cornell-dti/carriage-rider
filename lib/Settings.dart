285f5ed1a3f65e8229538d59c4e4a9e1a43b7a75
Tony Yang <txy3@cornell.edu>
2020-03-30 00:07:44 -0400

    set-up settings page

---
+++ lib/Settings.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'Login.dart';
+
+class Settings extends StatefulWidget {
+  Settings({Key key}) : super(key: key);
+
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Text('hello');
+  }
+}
