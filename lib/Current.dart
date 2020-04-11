f3ce4d6c41022b2bc36b3aace209cb12e35135f3
Tony Yang <txy3@cornell.edu>
2020-04-11 17:40:11 -0400

    remove bullet class

--- lib/Current.dart
+++ lib/Current.dart
@@ -704,31 +704,3 @@ class _RideStatusState extends State<RideStatus> {
             )));
   }
 }
-
-class Bullet extends Text {
-  const Bullet(
-    String data, {
-    Key key,
-    TextStyle style,
-    TextAlign textAlign,
-    TextDirection textDirection,
-    Locale locale,
-    bool softWrap,
-    TextOverflow overflow,
-    double textScaleFactor,
-    int maxLines,
-    String semanticsLabel,
-  }) : super(
-          '• $data',
-          key: key,
-          style: style,
-          textAlign: textAlign,
-          textDirection: textDirection,
-          locale: locale,
-          softWrap: softWrap,
-          overflow: overflow,
-          textScaleFactor: textScaleFactor,
-          maxLines: maxLines,
-          semanticsLabel: semanticsLabel,
-        );
-}
