35c6dbf5b26ea7537cd1f4055f9d95ee54524cdf
Jessie <jw823@cornell.edu>
2020-04-25 21:18:04 -0700

    UI of  Req Ride Form part1
    -added request req ride button in home page
    -UI of where to go completed
    (circle back different PR to add key/backend for form data)

--- lib/Request_Ride_Loc.dart
+++ lib/Request_Ride_Loc.dart
@@ -1,119 +1,129 @@
-import 'package:carriage_rider/Request_Ride_Time.dart';
 import 'package:flutter/material.dart';
 import 'package:carriage_rider/Home.dart';
+class RequestRideLoc extends StatefulWidget {
+  @override
+  _RequestRideLocState createState() => _RequestRideLocState();
+}
 
-class Request_Ride_Loc extends StatelessWidget {
+class _RequestRideLocState extends State<RequestRideLoc> {
+
+  Widget _buildFromField(){
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'From'),
+    );
+  }
+
+  Widget _buildToField(){
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'To'),
+    );
+  }
+
+  final cancelStyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w100,
+      fontSize: 15,
+  );
+
+  final fromToStyle = TextStyle(
+      color: Colors.grey,
+      fontWeight: FontWeight.w200,
+      fontSize: 12,
+  );
+
+  final questionStyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontSize: 25,
+  );
+
+  final locationStyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w600,
+      fontSize: 16,
+  );
+
   @override
   Widget build(BuildContext context) {
-    final cancelStyle = TextStyle(
-        color: Colors.black,
-        fontWeight: FontWeight.w100,
-        fontSize: 15,
-        height: 2
-    );
-
-    final fromToStyle = TextStyle(
-        color: Colors.grey,
-        fontWeight: FontWeight.w200,
-        fontSize: 12,
-        height: 2
-    );
-
-    final questionStyle = TextStyle(
-        color: Colors.black,
-        fontWeight: FontWeight.w800,
-        fontSize: 25,
-        height: 4
-    );
-
-    final locationStyle = TextStyle(
-        color: Colors.black,
-        fontWeight: FontWeight.w600,
-        fontSize: 16,
-        height: 2
-    );
-
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-              margin: EdgeInsets.only(right: 290.0, top: 45.0),
-              child: InkWell(
-                child: Text("Cancel", style: cancelStyle),
-                onTap: () {
-                  Navigator.pop(context, new MaterialPageRoute(builder: (context) => Home()));
-                },
+        body: Container(
+          margin: EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    child: InkWell(
+                      child: Text("Cancel", style: cancelStyle),
+                      onTap: () {
+                        Navigator.pop(context, new MaterialPageRoute(builder: (context) => Home()));
+                      },
+                    ),
+                  ),
+                ],
               ),
+              SizedBox(height: 50.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Icon(Icons.location_on),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 4.0),
+                    child: Icon(Icons.brightness_1, color: Colors.grey[350], size: 12.0),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 4.0),
+                    child: Icon(Icons.brightness_1, color: Colors.grey[350], size: 12.0),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 4.0),
+                    child: Icon(Icons.brightness_1, color: Colors.grey[350], size: 12.0),
+                  ),
+                  Icon(Icons.brightness_1, color: Colors.grey[350], size: 12.0),
+                ],
+              ),
+              SizedBox(height: 10.0),
+              Row(
+                children: <Widget>[
+                  Text("Where do you want to go?", style: questionStyle),
+                ],
+              ),
+              SizedBox(height: 40.0),
+              Form(
+                child: Column(
+                  children: <Widget>[
+                    _buildFromField(),
+                    SizedBox(height: 40.0),
+                    _buildToField(),
+                    SizedBox(height: 10.0)
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 20.0),
+                      child: ButtonTheme(
+                        minWidth: MediaQuery.of(context).size.width * 0.8,
+                        height: 45.0,
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(3)),
+                        child: RaisedButton(
+                          onPressed: (){},
+                          elevation: 3.0,
+                          color: Colors.black,
+                          textColor: Colors.white,
+                          child: Text('Next'),
+                        ),
+                      ),
+                    )
+                ),
+              )
+            ],
           ),
-          SizedBox(height: 70.0),
-          Container(
-            margin: EdgeInsets.only(left: 23.0),
-            child: Row(
-              children: <Widget>[
-                Icon(Icons.location_on),
-                Icon(Icons.remove_circle_outline),
-                Icon(Icons.remove_circle_outline),
-                Icon(Icons.remove_circle_outline),
-                Icon(Icons.remove_circle_outline)
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(),
-            margin: EdgeInsets.only(right: 25.0),
-            child: Text("Where do you want to go?", style: questionStyle),
-          ),
-          SizedBox(height: 10),
-          Container(
-            margin: EdgeInsets.only(right: 300.0),
-            child: Text("From", style: fromToStyle),
-          ),
-          Container(
-            margin: EdgeInsets.only(right: 145.0),
-            child: Text("Bill & Melinda Gates Hall", style: locationStyle),
-          ),
-          Container(
-            margin: EdgeInsets.only(right: 30.0, left: 30.0),
-            child: Divider(
-              height: 10,
-              thickness: 3,
-              color: Colors.black,
-            ),
-          ),
-          SizedBox(height: 10),
-          Container(
-            margin: EdgeInsets.only(right: 320.0),
-            child: Text("To", style: fromToStyle),
-          ),
-          Container(
-            margin: EdgeInsets.only(right: 245.0),
-            child: Text("Barton Hall", style: locationStyle),
-          ),
-          Container(
-            margin: EdgeInsets.only(right: 30.0, left: 30.0),
-            child: Divider(
-              height: 10,
-              thickness: 3,
-              color: Colors.black,
-            ),
-          ),
-          SizedBox(height: 240.0),
-          Container(
-            width: MediaQuery.of(context).size.width*0.8,
-            height: 50,
-            child: RaisedButton(
-              onPressed: () {
-                Navigator.push(context, new MaterialPageRoute(builder: (context) => RequestRideTime()));
-              },
-              child: Text('Next'),
-              textColor: Colors.white,
-              color: Colors.black,
-            ),
-          )
-
-        ],
-      ),
-
+        )
     );
+
   }
 }
