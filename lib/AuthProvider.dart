8c8e724997e11d736a24c2caad0d453d283e8056
chansen424 <chansen424@yahoo.com>
2020-04-28 21:12:02 -0400

    Imports

---
+++ lib/AuthProvider.dart
@@ -0,0 +1,9 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart';
+
+import 'Rider.dart';
+
