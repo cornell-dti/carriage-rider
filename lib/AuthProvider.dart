2ea9d1c8de10ac1b8535c64407b2f22d06619c08
chansen424 <chansen424@yahoo.com>
2020-04-28 21:13:00 -0400

    Moved over related functions
    These will be removed from there previous locations by the time a PR for this branch is submitted.

--- lib/AuthProvider.dart
+++ lib/AuthProvider.dart
@@ -7,3 +7,25 @@ import 'package:http/http.dart';
 
 import 'Rider.dart';
 
+Future<String> auth(String baseUrl, String token, String email) async {
+  String endpoint = baseUrl + '/auth';
+  Map<String, dynamic> requestBody = {
+    "token": token,
+    "email": email,
+    "clientID": "241748771473-0r3v31qcthi2kj09e5qk96mhsm5omrvr.apps.googleusercontent.com",
+    "table": "Riders"
+  };
+  return await post(endpoint, body: requestBody).then((res) {
+    return res.body;
+  });
+}
+
+Future<String> tokenFromAccount(GoogleSignInAccount account) async {
+  return await account.authentication.then((auth) {
+    return auth.idToken;
+  }).catchError((e) {
+    print('tokenFromAccount - $e');
+    return null;
+  });
+}
+
