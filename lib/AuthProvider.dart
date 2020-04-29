53cef9dcb1da8ccb3f3d960f965f4994be80e8ee
chansen424 <chansen424@yahoo.com>
2020-04-28 21:27:33 -0400

    Remove Rider import

--- lib/AuthProvider.dart
+++ lib/AuthProvider.dart
@@ -5,8 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart';
 
-import 'Rider.dart';
-
 Future<String> auth(String baseUrl, String token, String email) async {
   String endpoint = baseUrl + '/auth';
   Map<String, dynamic> requestBody = {
