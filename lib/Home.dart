34ba0f643c9c8dcf3b24403501bf0911fa679440
Jessie <jw823@cornell.edu>
2020-05-09 14:15:24 -0700

    delete import

--- lib/Home.dart
+++ lib/Home.dart
@@ -4,7 +4,6 @@ import 'package:carriage_rider/History.dart';
 import 'package:carriage_rider/Notifications.dart';
 import 'package:carriage_rider/Profile.dart';
 import 'package:carriage_rider/Request_Ride_Loc.dart';
-import 'package:carriage_rider/Settings.dart';
 import 'package:carriage_rider/Upcoming.dart';
 import 'package:flutter/material.dart';
 import 'package:carriage_rider/Ride_History.dart';
