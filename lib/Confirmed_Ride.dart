55120f8f219382428a1719bed8119830575e99fa
Tony <tony.yang98@gmail.com>
2020-09-14 20:49:13 -0400

    add phone calling feature

--- lib/Confirmed_Ride.dart
+++ lib/Confirmed_Ride.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:url_launcher/url_launcher.dart' as UrlLauncher;
 
 class ConfirmedRide extends StatefulWidget {
   @override
@@ -26,14 +26,323 @@ class _ConfirmedRideState extends State<ConfirmedRide> {
               children: <Widget>[
                 Column(
                   children: <Widget>[
-
+                    Header(header: 'OCT 18th 12:00 PM'),
+                    SizedBox(height: 5),
+                    SubHeader(subHeader1: 'Ride Info' , subHeader2: 'Confirmed'),
+                    SizedBox(height: 20),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      child: Row(
+                        children: <Widget>[
+                          SizedBox(width: 20),
+                          TimeLine(),
+                          SizedBox(width: 20),
+                          Expanded(
+                            child: Container(
+                              child: Column(
+                                children: <Widget>[
+                                  InformationRow(loc: 'Upson Hall', address: '109 Tower Rd, Ithaca, NY 14850', action: 'Pickup', time: '3:15 PM'),
+                                  SizedBox(height: 20),
+                                  InformationRow(loc: 'Gates Hall', address: '107 Hoy Rd, Ithaca, NY 14850', action: 'Dropoff', time: '3:20 PM'),
+                                  SizedBox(height: 20)
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 20)
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    CustomDivider(),
+                    SizedBox(height: 15),
+                    Contact(),
+                    SizedBox(height: 15),
+                    CustomDivider(),
+                    SizedBox(height: 20),
+                    RideAction(rideAction: "Cancel Ride", color: Colors.red, icon: Icons.clear),
+                    SizedBox(height: MediaQuery.of(context).size.height/8),
                   ],
                 )
               ],
             ),
-
+            EditRide()
           ],
         )
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+
+  const Header({Key key, this.header}) : super(key: key);
+  final String header;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(left: 15, top: 15, bottom: 15),
+          child: Text(header,
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontFamily: 'SFPro',
+                  fontWeight: FontWeight.bold)),
+        )
+      ],
+    );
+  }
+}
+
+class SubHeader extends StatelessWidget {
+
+  const SubHeader({Key key, this.subHeader1, this.subHeader2}) : super(key: key);
+
+  final String subHeader1;
+  final String subHeader2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 15),
+      child: Row(
+        children: <Widget>[
+          Text(subHeader1, style: TextStyle(color: Colors.grey[800],
+              fontSize: 20,
+              fontFamily: 'SFPro',
+              fontWeight: FontWeight.bold)),
+          SizedBox(width: 10.0),
+          Text(subHeader2,
+              style: TextStyle(color: Colors.green[500], fontSize: 11))
+        ],
+      ),
+    );
+  }
+}
+
+class CustomDivider extends StatelessWidget {
+
+  const CustomDivider({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 4,
+      color: Colors.grey[200],
+    );
+  }
+}
+
+class Contact extends StatelessWidget {
+
+  const Contact({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 15),
+      child: Row(
+        children: <Widget>[
+          Icon(Icons.account_circle, size: 50),
+          SizedBox(width: 15),
+          Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('Davea Butler', style: TextStyle(color: Colors.black,
+                    fontSize: 16,
+                    fontFamily: 'SFPro')),
+                SizedBox(height: 5),
+                Container(
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.phone, size: 15),
+                      SizedBox(width: 7),
+                      GestureDetector(
+                        onTap: () => UrlLauncher.launch("tel://21213123123"),
+                        child: Text('+1 323-231-5234', style: TextStyle(color: Colors.grey[600])),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class EditRide extends StatelessWidget {
+
+  const EditRide({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height/8,
+        color: Colors.white,
+        child: Stack(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.center,
+              child: ButtonTheme(
+                minWidth: MediaQuery.of(context).size.width * 0.8,
+                height: 45.0,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(3)),
+                child: RaisedButton.icon(
+                    onPressed: (){},
+                    elevation: 3.0,
+                    color: Colors.black,
+                    textColor: Colors.white,
+                    icon: Icon(Icons.mode_edit),
+                    label: Text('Edit Ride')
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TimeLine extends StatelessWidget {
+
+  const TimeLine({Key key}) : super(key: key);
+
+  Widget locationCircle() {
+    return Container(
+      width: 18,
+      height: 18,
+      decoration: new BoxDecoration(
+        color: Colors.black,
+        shape: BoxShape.circle,
+        border: new Border.all(
+            color: Colors.white, width: 6.0, style: BorderStyle.solid),
+        boxShadow: [
+          new BoxShadow(
+            color: Colors.grey[900],
+            blurRadius: 5.0,
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 18,
+      child: Column(
+        children: <Widget>[
+          Container(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  child: Stack(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.all(10.0),
+                        margin: EdgeInsets.only(left: 6),
+                        width: 4,
+                        height: 80,
+                        color: Colors.black,
+                      ),
+                      locationCircle(),
+                      Positioned(
+                        top: 60,
+                        child: locationCircle(),
+                      ),
+
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class InformationRow extends StatelessWidget {
+
+  const InformationRow({Key key, this.loc, this.address, this.action, this.time}) : super(key: key);
+
+  final String loc;
+  final String address;
+  final String action;
+  final String time;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(loc,
+                style: TextStyle(color: Colors.black,
+                    fontSize: 18,
+                    fontFamily: 'SFPro')
+            ),
+            Text(action,
+                style: TextStyle(color: Colors.grey,
+                    fontSize: 13,
+                    fontFamily: 'SFPro')
+            ),
+          ],
+        ),
+        SizedBox(height: 5.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(address,
+                style: TextStyle(color: Colors.grey,
+                    fontSize: 12,
+                    fontFamily: 'SFPro')
+            ),
+            Text(time,
+                style: TextStyle(color: Colors.grey,
+                    fontSize: 12,
+                    fontFamily: 'SFPro')
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class RideAction extends StatelessWidget {
+
+  const RideAction({Key key, this.rideAction, this.color, this.icon}) : super(key: key);
+
+  final String rideAction;
+  final Color color;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(icon, color: color),
+        SizedBox(width: 10),
+        Text(rideAction, style: TextStyle(color: color,
+            fontSize: 18,
+            fontFamily: 'SFPro'))
+      ],
     );
   }
 }
