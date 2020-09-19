d205474420de70ab8fa82b646ac965cb078ad3c9
Jessie <jw823@cornell.edu>
2020-09-19 15:05:07 -0700

    Created Custom Widgets
    Created Custom Widgets to make code more readable and to easily repeat History Cards.

--- lib/Ride_History.dart
+++ lib/Ride_History.dart
@@ -4,185 +4,153 @@ class RideHistory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    final directionStyle = TextStyle(
+    return Column(
+      children: <Widget>[
+        RideHistoryCard(timeDateWidget: TimeDateHeader(timeDate: 'OCT 18th 12:00 PM'),
+            infoRowWidget: InformationRow(start: 'Upson Hall', end: 'Uris Hall'),),
+        RideHistoryCard(timeDateWidget: TimeDateHeader(timeDate: 'FEB 29th 12:00 PM'),
+          infoRowWidget: InformationRow(start: 'Balch Hall', end: 'Gates Hall'),)
+      ],
+    );
+  }
+}
+
+class TimeDateHeader extends StatelessWidget{
+
+  const TimeDateHeader({Key key, this.timeDate}) : super(key: key);
+
+  final String timeDate;
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    final timeDateStyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+      fontFamily: 'SFPro',
+      fontSize: 22,
+    );
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(left: 25),
+          child: Text(timeDate, style: timeDateStyle),
+        )
+      ],
+    );
+  }
+
+}
+
+class InformationRow extends StatelessWidget {
+
+  const InformationRow({Key key, this.start, this.end}) : super(key: key);
+
+  final start;
+  final end;
+
+  @override
+  Widget build(BuildContext context) {
+    final fromToStyle = TextStyle(
         color: Colors.grey[500],
         fontWeight: FontWeight.w500,
-        letterSpacing: 0.2,
         fontSize: 12,
-        height: 2
     );
 
     final infoStyle = TextStyle(
         color: Colors.black,
         fontWeight: FontWeight.w400,
-        letterSpacing: 0.2,
         fontSize: 18,
-        height: 2
     );
-
-    final monthStyle = TextStyle(
-        color: Colors.black,
-        fontWeight: FontWeight.w700,
-        letterSpacing: 0.2,
-        fontSize: 18,
-        height: 2
-    );
-
-    final dayStyle = TextStyle(
-        color: Colors.grey[700],
-        fontWeight: FontWeight.w500,
-        letterSpacing: 0.2,
-        fontSize: 18,
-        height: 2
-    );
-
-    final timeStyle = TextStyle(
-        color: Colors.grey[500],
-        fontWeight: FontWeight.w400,
-        letterSpacing: 0.2,
-        fontSize: 17,
-        height: 2
-    );
-
-    return Column(
-      children: <Widget>[
-        Container(
-          margin: EdgeInsets.only(left: 17.0, right: 17.0, bottom: 15.0),
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height * .16,
-          child: Card(
-            elevation: 3.0,
+    return Container(
+      margin: EdgeInsets.only(left: 25, right: 25),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 15.0),
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(right: 2.0),
-                        child: Text('OCT', style: monthStyle),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(right: 2.0, top: 1.0),
-                        child: Text('18th', style: dayStyle),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(right: 2.0, top: 3.0),
-                        child: Text('12:00 PM', style: timeStyle),
-                      )
-                    ],
-                  ),
+                Row(
+                  children: <Widget>[
+                    Text('From', style: fromToStyle)
+                  ],
                 ),
-                Container(
-                  margin: EdgeInsets.only(left: 5.0, right: 5.0),
-                  child: Row(
-                    children: <Widget>[
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Padding(
-                            padding: EdgeInsets.only(left: 11.0),
-                            child: Text('From', style: directionStyle),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(left: 11.0),
-                            child: Text('Upson Hall', style: infoStyle),
-                          )
-                        ],
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 50.0),
-                        child: Icon(Icons.arrow_forward),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Padding(
-                            padding: EdgeInsets.only(left: 66.0),
-                            child: Text('To', style: directionStyle),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(left: 66.0),
-                            child: Text('Uris Hall', style: infoStyle),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
+                SizedBox(height: 2),
+                Row(
+                  children: <Widget>[
+                    Text(start, style: infoStyle)
+                  ],
                 )
               ],
             ),
           ),
-        ),
-        Container(
-          margin: EdgeInsets.only(left: 17.0, right: 17.0, bottom: 15.0),
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height * .16,
-          child: Card(
-            elevation: 3.0,
+          Container(
             child: Column(
               children: <Widget>[
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 15.0),
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(right: 2.0),
-                        child: Text('MAR', style: monthStyle),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(right: 2.0, top: 1.0),
-                        child: Text('3rd', style: dayStyle),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(right: 2.0, top: 3.0),
-                        child: Text('3:00 PM', style: timeStyle),
-                      )
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 5.0, right: 5.0),
-                  child: Row(
-                    children: <Widget>[
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Padding(
-                            padding: EdgeInsets.only(left: 11.0),
-                            child: Text('From', style: directionStyle),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(left: 11.0),
-                            child: Text('Balch Hall', style: infoStyle),
-                          )
-                        ],
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 50.0),
-                        child: Icon(Icons.arrow_forward),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Padding(
-                            padding: EdgeInsets.only(left: 68.0),
-                            child: Text('To', style: directionStyle),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(left: 68.0),
-                            child: Text('Gates Hall', style: infoStyle),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.arrow_forward)
+                  ],
                 )
               ],
             ),
           ),
-        )
-      ],
+          Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Text('To', style: fromToStyle)
+                  ],
+                ),
+                SizedBox(height: 5),
+                Row(
+                  children: <Widget>[
+                    Text(end, style: infoStyle)
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+
+      ),
     );
   }
 }
+
+class RideHistoryCard extends StatelessWidget {
+
+  const RideHistoryCard({Key key, this.timeDateWidget, this.infoRowWidget}) : super(key: key);
+
+  final Widget timeDateWidget;
+  final Widget infoRowWidget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 17.0, right: 17.0, bottom: 15.0),
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * .18,
+      child: Card(
+        elevation: 3.0,
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              timeDateWidget,
+              infoRowWidget
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
