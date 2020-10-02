a8e7c1ad8879135547ead4404b070412b44592ad
Tony <tony.yang98@gmail.com>
2020-10-02 02:53:02 -0400

    remove unused import
    -fix build issue

--- lib/Current.dart
+++ lib/Current.dart
@@ -1,6 +1,5 @@
 import 'dart:ui';
 import 'package:carriage_rider/AuthProvider.dart';
-import 'package:carriage_rider/Upcoming.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'size_config.dart';
