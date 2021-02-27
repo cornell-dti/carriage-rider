f74e0a428a039421e76692e4c86e606f1d1b0472
Tony Yang <txy3@cornell.edu>
2021-02-27 03:05:22 -0500

    Update Location.dart

--- lib/models/Location.dart
+++ lib/models/Location.dart
@@ -27,7 +27,7 @@ Future<List<Location>> fetchLocations(BuildContext context) async {
   final response = await http.get(AppConfig.of(context).baseUrl + '/locations');
   if (response.statusCode == 200) {
     String responseBody = response.body;
-    var data = jsonDecode(responseBody)["data"];
+    var data = jsonDecode(responseBody)['data'];
     List<Location> locations =
         data.map<Location>((e) => Location.fromJson(e)).toList();
     return locations;
