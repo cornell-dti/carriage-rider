0ff39b912a116432a950cca89414a8685e60a4b2
Tony Yang <txy3@cornell.edu>
2021-05-08 17:21:19 -0400

    remove unused code

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:carriage_rider/pages/OnBoarding.dart';
 import 'package:carriage_rider/providers/AuthProvider.dart';
 import 'package:carriage_rider/providers/RideFlowProvider.dart';
@@ -10,7 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'pages/Login.dart';
 import 'pages/Home.dart';
-import 'package:overlay_support/overlay_support.dart';
 import 'utils/app_config.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -54,50 +52,48 @@ class MyApp extends StatelessWidget {
                   create: (context) {
                     return RideFlowProvider();
                   },
-                  child: OverlaySupport(
-                    child: MaterialApp(
-                      title: 'Carriage Rider',
-                      theme: ThemeData(
-                          primarySwatch: Colors.green,
-                          fontFamily: 'SFPro',
-                          accentColor: Color.fromRGBO(60, 60, 67, 0.6),
-                          textTheme: TextTheme(
-                            headline4: TextStyle(
-                                fontFamily: 'SFDisplay',
-                                fontSize: 34,
-                                fontWeight: FontWeight.bold,
-                                letterSpacing: 0.37,
-                                color: Colors.black),
-                            headline5: TextStyle(
-                                fontFamily: 'SFDisplay',
-                                fontSize: 28,
-                                fontWeight: FontWeight.w700,
-                                letterSpacing: 0.23,
-                                color: Colors.black),
-                            headline6: TextStyle(
-                                fontFamily: 'SFDisplay',
-                                fontSize: 20.0,
-                                fontWeight: FontWeight.w700,
-                                letterSpacing: 0.38,
-                                color: Colors.black),
-                            subtitle2: TextStyle(
-                                fontSize: 17.0,
-                                fontWeight: FontWeight.bold,
-                                letterSpacing: -0.41),
-                            bodyText1: TextStyle(
-                                fontSize: 16.0, fontWeight: FontWeight.normal),
-                            bodyText2: TextStyle(
-                                fontSize: 12.0, fontWeight: FontWeight.normal),
-                            headline1: TextStyle(
-                                color: Colors.black,
-                                fontSize: 34,
-                                fontWeight: FontWeight.bold),
-                            subtitle1: TextStyle(
-                                fontSize: 17.0, fontWeight: FontWeight.bold),
-                          )),
-                      home: Logic(),
-                      debugShowCheckedModeBanner: false,
-                    ),
+                  child: MaterialApp(
+                    title: 'Carriage Rider',
+                    theme: ThemeData(
+                        primarySwatch: Colors.green,
+                        fontFamily: 'SFPro',
+                        accentColor: Color.fromRGBO(60, 60, 67, 0.6),
+                        textTheme: TextTheme(
+                          headline4: TextStyle(
+                              fontFamily: 'SFDisplay',
+                              fontSize: 34,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 0.37,
+                              color: Colors.black),
+                          headline5: TextStyle(
+                              fontFamily: 'SFDisplay',
+                              fontSize: 28,
+                              fontWeight: FontWeight.w700,
+                              letterSpacing: 0.23,
+                              color: Colors.black),
+                          headline6: TextStyle(
+                              fontFamily: 'SFDisplay',
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.w700,
+                              letterSpacing: 0.38,
+                              color: Colors.black),
+                          subtitle2: TextStyle(
+                              fontSize: 17.0,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: -0.41),
+                          bodyText1: TextStyle(
+                              fontSize: 16.0, fontWeight: FontWeight.normal),
+                          bodyText2: TextStyle(
+                              fontSize: 12.0, fontWeight: FontWeight.normal),
+                          headline1: TextStyle(
+                              color: Colors.black,
+                              fontSize: 34,
+                              fontWeight: FontWeight.bold),
+                          subtitle1: TextStyle(
+                              fontSize: 17.0, fontWeight: FontWeight.bold),
+                        )),
+                    home: Logic(),
+                    debugShowCheckedModeBanner: false,
                   ))),
         ),
       ),
--- lib/utils/NotificationService.dart
+++ lib/utils/NotificationService.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class PushNotificationMessageAndroid {
   final String title;
   final String body;
+
   PushNotificationMessageAndroid({
     @required this.title,
     @required this.body,
@@ -20,6 +21,7 @@ class PushNotificationMessageAndroid {
 class PushNotificationMessageIOS {
   final String title;
   final String body;
+
   PushNotificationMessageIOS({
     @required this.title,
     @required this.body,
@@ -29,34 +31,6 @@ class PushNotificationMessageIOS {
     return PushNotificationMessageIOS(
       title: json['aps']['alert']['title'],
       body: json['aps']['alert']['body'],
-    );
-  }
-}
-
-
-class NotificationBadge extends StatelessWidget {
-  final int totalNotifications;
-
-  const NotificationBadge({@required this.totalNotifications});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 40.0,
-      height: 40.0,
-      decoration: new BoxDecoration(
-        color: Colors.red,
-        shape: BoxShape.circle,
-      ),
-      child: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            '$totalNotifications',
-            style: TextStyle(color: Colors.white, fontSize: 20),
-          ),
-        ),
-      ),
     );
   }
 }
