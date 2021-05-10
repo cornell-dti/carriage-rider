42943ea6c1767f7d384681f927031c63223ee933
helenxuyang <helen.xu.yang@gmail.com>
2021-05-10 01:30:25 -0400

    Fixed ScheduleBar to be accessible both with and without screen reader

--- lib/widgets/ScheduleBar.dart
+++ lib/widgets/ScheduleBar.dart
@@ -8,28 +8,35 @@ class ScheduleBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Semantics(
-      button: true,
-      label: 'Return to schedule',
-      onTap: () => Navigator.pop(context, false),
-      child: ExcludeSemantics(
-        child: AppBar(
-          backgroundColor: backgroundColor,
-          titleSpacing: 0.0,
-          elevation: 0,
-          automaticallyImplyLeading: false,
-          title: Row(
-            children: [
-              SizedBox(width: 16),
-              Icon(Icons.arrow_back_ios, color: textColor),
-              ExcludeSemantics(
-                child: Text(
-                  'Schedule',
-                  style: TextStyle(
-                      color: textColor, fontSize: 20, fontFamily: 'SFPro'),
-                ),
+    return AppBar(
+      backgroundColor: backgroundColor,
+      titleSpacing: 0.0,
+      elevation: 0,
+      automaticallyImplyLeading: false,
+      title: Container(
+        height: 48,
+        child: InkWell(
+          onTap: () => Navigator.pop(context, false),
+          child: Semantics(
+            button: true,
+            label: 'Return to schedule',
+            onTap: () => Navigator.pop(context, false),
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.arrow_back_ios, color: textColor),
+                  ExcludeSemantics(
+                    child: Text(
+                      'Schedule',
+                      style: TextStyle(
+                          color: textColor, fontSize: 20, fontFamily: 'SFPro'),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
