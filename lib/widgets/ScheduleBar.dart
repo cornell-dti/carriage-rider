8d721ee9d2abf18c6110ac821d6c7c4076d7a100
helenxuyang <helen.xu.yang@gmail.com>
2021-05-09 23:45:58 -0400

    Fixed ScheduleBar semantics to say it's a button and read 'return to schedule' for more description

--- lib/widgets/ScheduleBar.dart
+++ lib/widgets/ScheduleBar.dart
@@ -8,23 +8,29 @@ class ScheduleBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      backgroundColor: backgroundColor,
-      titleSpacing: 0.0,
-      elevation: 0,
-      automaticallyImplyLeading: false,
-      title: GestureDetector(
-        onTap: () => Navigator.pop(context, false),
-        child: Row(
-          children: [
-            SizedBox(width: 16),
-            Icon(Icons.arrow_back_ios, color: textColor),
-            Text(
-              'Schedule',
-              style: TextStyle(
-                  color: textColor, fontSize: 20, fontFamily: 'SFPro'),
-            ),
-          ],
+    return Semantics(
+      button: true,
+      label: 'Return to schedule',
+      onTap: () => Navigator.pop(context, false),
+      child: ExcludeSemantics(
+        child: AppBar(
+          backgroundColor: backgroundColor,
+          titleSpacing: 0.0,
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          title: Row(
+            children: [
+              SizedBox(width: 16),
+              Icon(Icons.arrow_back_ios, color: textColor),
+              ExcludeSemantics(
+                child: Text(
+                  'Schedule',
+                  style: TextStyle(
+                      color: textColor, fontSize: 20, fontFamily: 'SFPro'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
