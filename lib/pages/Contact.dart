38d3ec0f54b19ed4f74c38ce8dcaf0d5980bf4f1
helenxuyang <helen.xu.yang@gmail.com>
2021-05-12 03:41:24 -0400

    Added contact page + its semantics

--- lib/pages/Contact.dart
+++ lib/pages/Contact.dart
@@ -10,106 +10,120 @@ class Contact extends StatelessWidget {
     return Scaffold(
         appBar: ScheduleBar(Colors.black, Theme.of(context).scaffoldBackgroundColor),
         body: SingleChildScrollView(
-          child:
-          Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
-              Widget>[
-            Padding(
-              padding: EdgeInsets.only(left: 15.0, top: 5.0, bottom: 8.0),
-              child: Text('Contact', style: CarriageTheme.largeTitle),
-            ),
-            Container(
-              color: Colors.white,
-              height: MediaQuery
-                  .of(context)
-                  .size
-                  .height,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(top: 20, left: 18, bottom: 5),
-                    child: Text(
-                      'Contact CULift',
-                      style: CarriageTheme.title2,
-                    ),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Semantics(
+                  header: true,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 15.0, top: 5.0, bottom: 8.0),
+                    child: Text('Contact CULift', style: CarriageTheme.largeTitle),
                   ),
-                  Flexible(
-                    child: Padding(
-                      padding: EdgeInsets.only(top: 10, left: 18, bottom: 15),
+                ),
+                Container(
+                  color: Colors.white,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      SizedBox(height: 8),
+                      PhoneNumberRow('Questions about scheduled rides can be made by calling the CULift dispatcher at:', '6072548293'),
+                      ContactRow(Icons.mail, "If it's before 3:45pm, you can also send an email to:", 'culift@cornell.edu', 'mailto:culift@cornell.edu'),
+                      PhoneNumberRow('For night rides after 3:45pm, call the CULift dispatcher at:', '6072296010'),
+                    ],
+                  ),
+                )
+              ]
+          ),
+        )
+    );
+  }
+}
 
-                      child: Text(
-                        'Did your driver miss your ride? Or need any immediate '
-                            'assistance? Contact CULift for help',
-                        style: TextStyle(
-                          color: Colors.grey,
-                        ),
+class ContactRow extends StatelessWidget {
+  ContactRow(this.icon, this.intro, this.text, this.url, {this.isPhoneNumber = false});
+  final IconData icon;
+  final String intro;
+  final String text;
+  final String url;
+  final bool isPhoneNumber;
+
+  @override
+  Widget build(BuildContext context) {
+    return Semantics(
+      button: true,
+      child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(intro,
+                style: CarriageTheme.body
+              ),
+              Row(
+                children: <Widget>[
+                  Icon(icon),
+                  SizedBox(width: 19),
+                  Expanded(
+                    child: Text(
+                      text,
+                      semanticsLabel: isPhoneNumber ? text.characters.fold('', (previousValue, element) => previousValue + ' ' + element) : text,
+                      style: TextStyle(
+                        fontSize: 17,
+                        color: CarriageTheme.gray1,
                       ),
                     ),
                   ),
-                  infoRow(
-                      context, Icons.phone, '###-###-####', _launchPhoneURL),
-                  Padding(
-                    padding: EdgeInsets.only(left: 18),
-                    child: Divider(
-                      height: 0,
-                      color: Colors.black,
-                    ),
-                  ),
-                  infoRow(context, Icons.mail_outline, 'culift@cornell.edu',
-                      _launchMailURL),
+                  ArrowURLButton(url)
                 ],
               ),
-            )
-          ]),
-        ));
-  }
-
-  void _launchPhoneURL() async {
-    String number = '13232315234';
-    String url = 'tel://$number';
-    if (await UrlLauncher.canLaunch(url)) {
-      await UrlLauncher.launch(url);
-    } else {
-      print('Could not launch $url');
-    }
-  }
-
-  void _launchMailURL() async {
-    final Uri params = Uri(
-      scheme: 'mailto',
-      path: 'culift@cornell.edu',
+              Divider()
+            ],
+          )
+      ),
     );
-
-    String url = params.toString();
-    if (await UrlLauncher.canLaunch(url)) {
-      await UrlLauncher.launch(url);
-    } else {
-      print('Could not launch $url');
-    }
   }
+}
 
-  Widget infoRow(BuildContext context, IconData icon, String text,
-      void Function() onPressed) {
-    return Padding(
-        padding: EdgeInsets.only(left: 18),
-        child: Row(
-          children: <Widget>[
-            Icon(icon),
-            SizedBox(width: 19),
-            Expanded(
-              child: Text(
-                text,
-                style: TextStyle(
-                  fontSize: 17,
-                  color: Colors.grey[500],
-                ),
-              ),
+class PhoneNumberRow extends StatelessWidget {
+  PhoneNumberRow(this.text, this.phoneNumber);
+  final String text;
+  final String phoneNumber;
+
+  @override
+  Widget build(BuildContext context) {
+    String phoneNumberFormatted = '(${phoneNumber.substring(0,3)}) ${phoneNumber.substring(3,6)}-${phoneNumber.substring(6)}';
+    return ContactRow(Icons.phone, text, phoneNumberFormatted, 'tel:' + phoneNumber, isPhoneNumber: true);
+  }
+}
+
+class ArrowURLButton extends StatelessWidget {
+  ArrowURLButton(this.url);
+  final String url;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(100),
+        ),
+        child: Material(
+          type: MaterialType.transparency,
+          child: InkWell(
+            customBorder: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Icon(Icons.arrow_forward_ios, size: 20),
             ),
-            IconButton(
-              icon: Icon(Icons.arrow_forward_ios),
-              onPressed: onPressed,
-            )
-          ],
-        ));
+            onTap: () async {
+              if (await UrlLauncher.canLaunch(url)) {
+                await UrlLauncher.launch(url);
+              } else {
+                print('Could not launch $url');
+              }
+            },
+          ),
+        )
+    );
   }
 }
