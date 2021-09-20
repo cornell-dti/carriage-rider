606b7c532d90f3cccbf67d1b0fc68ea860dd0352
Helen Yang <helen.xu.yang@gmail.com>
2021-09-19 23:52:18 -0400

    Removed unused math dep

--- lib/models/Ride.dart
+++ lib/models/Ride.dart
@@ -1,5 +1,4 @@
 import 'dart:core';
-import 'dart:math';
 import 'package:carriage_rider/providers/RiderProvider.dart';
 import 'package:carriage_rider/utils/Ordinal.dart';
 import 'package:flutter/material.dart';
