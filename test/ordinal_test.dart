5e68b081e6ae8c3cd9d057fd4d3d0a0b782fc96c
Helen Yang <helen.xu.yang@gmail.com>
2021-09-19 23:55:59 -0400

    Ran format

--- test/ordinal_test.dart
+++ test/ordinal_test.dart
@@ -5,12 +5,41 @@ void main() {
   test('Ordinal function returns correct strings', () {
     // tests from 1 to 31 because we use ordinal for days in dates
     List<String> ordinals = [
-      '1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th',
-      '11th', '12th', '13th', '14th', '15th', '16th', '17th', '18th', '19th', '20th',
-      '21st', '22nd', '23rd', '24th', '25th', '26th', '27th', '28th', '29th', '30th', '31st'];
+      '1st',
+      '2nd',
+      '3rd',
+      '4th',
+      '5th',
+      '6th',
+      '7th',
+      '8th',
+      '9th',
+      '10th',
+      '11th',
+      '12th',
+      '13th',
+      '14th',
+      '15th',
+      '16th',
+      '17th',
+      '18th',
+      '19th',
+      '20th',
+      '21st',
+      '22nd',
+      '23rd',
+      '24th',
+      '25th',
+      '26th',
+      '27th',
+      '28th',
+      '29th',
+      '30th',
+      '31st'
+    ];
 
     for (int i = 1; i <= 31; i++) {
-      expect(ordinal(i), ordinals[i-1]);
+      expect(ordinal(i), ordinals[i - 1]);
     }
   });
 }
