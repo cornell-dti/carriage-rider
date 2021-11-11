cf3734bda8a7ccf4a5f4b7383c6371248b949c05
Matthew Guo <mguo22@gmail.com>
2021-11-10 23:49:54 -0500

    fix background handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'utils/app_config.dart';
 import 'main_common.dart';
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  String notifId = message.data['id'];
+  print('Handling a background message $notifId');
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   AppConfig configuredApp = AppConfig(
     baseUrl: 'https://carriage-web.herokuapp.com/api',
