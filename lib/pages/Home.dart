accad316795e4427fe3f9899a009976da7948de5
Matthew Guo <mguo22@gmail.com>
2021-11-16 19:37:31 -0500

    Update Home.dart

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -297,7 +297,7 @@ class _HomeState extends State<Home> {
           getNotifEventEnum(data['notifEvent']),
           notification.body,
           ride.id,
-          message.sentTime);
+          DateTime.parse(data['sentTime']));
       notifsProvider.addNewNotif(backendNotif);
 
       if (android != null) {
