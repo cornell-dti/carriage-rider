7bd2a12de925da363e845cde2dc713cf07919dff
Pratyush Sudhakar <pratyushsudhakar03@gmail.com>
2023-03-24 01:36:23 -0400

    use localhost

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ void main() async {
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   AppConfig configuredApp = AppConfig(
-    baseUrl: 'https://carriage-web.herokuapp.com/api',
+    baseUrl: 'https://10.0.2.2:3001/api',
     child: MyApp(),
   );
 
